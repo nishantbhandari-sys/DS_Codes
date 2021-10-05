13bc5f789e720eadf00e896797881a7d683ffa12
nishant-out <79033833+nishant-out@users.noreply.github.com>
2021-10-05 22:18:56 +0530

    Update DEQue.c

--- DEQue.c
+++ DEQue.c
@@ -58,13 +58,88 @@ void front_push( void ){
     
     arr[ front ] = val ;
 }
+void rear_push( void ){
+    int val ;
+    printf( "\nEnter val to insert: " ) ;
+    scanf( "%d", &val ) ;
+    
+    if( (front == 0 && rear == siz -1) || front == rear +1 ){
+        printf( "\nQueue Full" ) ;
+        return ;
+    }
+    
+    if( rear == -1 ){
+        front = 0 ;
+        rear  = 0 ;
+    }
+    else{
+        if( rear == siz -1 )
+            rear = 0 ;
+        else
+            rear ++ ;
+    }
+    arr[ rear ] = val ;
+}
+void front_del( void ){
+    if( front == -1  ){ // Stack is Empty
+        printf( "\nDEque Empty" ) ;
+        return ;
+    }
+    if( front == rear ){
+        front = -1 ;
+        rear  = -1 ;
+    }
+    else if( front == siz -1 ){
+        front = 0 ;
+    }
+    else{
+        front ++ ;
+    }
+}
+void rear_del( void ){
+    if( front == -1  ){ // Stack is Empty   or      rear == -1
+        printf( "\nDEque Empty" ) ;
+        return ;
+    }
+    if( rear == front ){    //  check for last Element
+        front = -1 ;
+        rear  = -1 ;
+    }  
+    else if( rear == 0 ){
+        rear = siz -1 ;
+    }
+    else
+        rear -- ;
+}
 
 int main()
 {
     front_push( ) ;
+    printf( "\nfront = %d, rear = %d\n", front, rear ) ;
+
     front_push( ) ;
+    printf( "\nfront = %d, rear = %d\n", front, rear ) ;
+    
     front_push( ) ;
+    printf( "\nfront = %d, rear = %d\n", front, rear ) ;
+    
+    front_push( ) ;
+    printf( "\nfront = %d, rear = %d\n", front, rear ) ;
+    
+    front_push( ) ;
+    printf( "\nfront = %d, rear = %d\n", front, rear ) ;
+    display( ) ;
+    
+    rear_del( ) ;
+    printf( "\nfront = %d, rear = %d\n", front, rear ) ;
+    rear_del( ) ;
+    printf( "\nfront = %d, rear = %d\n", front, rear ) ;
+    rear_del( ) ;
+    printf( "\nfront = %d, rear = %d\n", front, rear ) ;
+    // rear_push( ) ;
+    // printf( "\nfront = %d, rear = %d\n", front, rear ) ;
 
     display( ) ;
     return 0;
 }
+
