5651bebb53aa85c73d1015e2c91518c16b529db0
nishant-out <79033833+nishant-out@users.noreply.github.com>
2021-10-18 11:56:17 +0530

    Update priority_Queue.c

--- priority_Queue.c
+++ priority_Queue.c
@@ -1,24 +1,102 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 typedef struct{
     int data ;
     int prty ;
+    
 } Que ;
+
+void display( Que *qp, int front, int rear ){
+    if( rear == -1 ){
+        printf( "QEmpty" ) ;
+        return ;
+    }
+    printf( "\nQueue Elements: " ) ;
+    int f = front ;
+    while( f <= rear ){
+        printf( "%d ", qp[ f ].data ) ;
+        f++ ;
+    }
+    printf( "\n" ) ;
+    
+    printf( "\nQueue Priority: " ) ;
+    f = front ;
+    while( f <= rear ){
+        printf( "%d ", qp[ f ].prty ) ;
+        f++ ;
+    }
+    printf( "\n" ) ;
+}
+
+Que* pop( Que* q, int *f, int *r ){
+    if( *r == -1 ){
+        printf( "\nQEmpty" ) ;
+        return q ;
+    }
+    
+    //  Calc lowest priority
+    int pp = INT_MAX ;
+    for( int i = 0; i <= *r; i++ ){
+        if( q[ i ].prty < pp ){
+            pp = q[ i ].prty ;
+        }
+    }
+    
+    Que* temp = (Que*) malloc( sizeof(Que) * (*r) ) ;
+    
+    int i = 0 ;
+    while( q[ i ].prty != pp ){
+        
+        temp[ i ].data = q[ i ].data ;
+        temp[ i ].prty = q[ i ].prty ;
+        i++ ;
+    }
+    
+    ++i ;   //  skip this element
+    while( i <= *r ){
+        
+        temp[ i -1].data = q[ i ].data ;
+        temp[ i -1].prty = q[ i ].prty ;
+        i++ ;
+    }
+    
+    (*r)-- ;
+    free( q ) ;
+
+    return temp ;
+}
 
 Que* push( Que* q, int *f, int *r ){
     
     int val , priority ;
-    printf( "\nEnter Data and priority" ) ;
+    printf( "\nEnter Data and priority: " ) ;
     scanf( "%d %d", &val, &priority ) ;
     
+    if( *r == -1 ){
+        *f = 0 ;
+    }
     
+    Que* temp = (Que*) malloc( sizeof(Que) * (*r +2) ) ;
+    for( int i = 0; i <= *r; i++ ){
+        temp[ i ].data = q[ i ].data ;
+        temp[ i ].prty = q[ i ].prty ;
+    }
+    (*r)++ ;
+    //  Add new element and it's priority
+    temp[ *r ].data = val ;
+    temp[ *r ].prty = priority ;
+    
+    free( q ) ;
+    return temp ;
 }
 
 int main()
 {
-    printf( "1. push" ) ;
-    printf( "2. pop" ) ;
-    printf( "3. display" ) ;
+    printf( "1. push\n" ) ;
+    printf( "2. pop\n" ) ;
+    printf( "3. display\n" ) ;
     
     Que* q = NULL ;
     int front = -1 ;
@@ -31,11 +109,11 @@ int main()
         
         switch( ch ){
             
-            case 1: push( q, &front, &rear ) ;      break ;
+            case 1: q = push( q, &front, &rear ) ;      break ;
             
-            case 1: push( q, &front, &rear ) ;      break ;
+            case 2: q = pop( q, &front, &rear ) ;      break ;
             
-            case 1: push( q, &front, &rear ) ;      break ;
+            case 3: display( q, front, rear ) ;      break ;
             
             default:    ch = -1 ;
         }
