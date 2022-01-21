cedfbeafb9035a27375822249e273fe17495b6f5
Nishant Bhandari <79033833+nishant-out@users.noreply.github.com>
2022-01-21 14:07:22 +0000

    Update BST_Without_P.c

--- BST_Without_P.c
+++ BST_Without_P.c
@@ -9,10 +9,6 @@ typedef struct {
 
 Stack* push( Stack *ptr, int *top, int val ){
 
-// 	int val ;
-// 	printf( "Enter Element in Stack: " ) ;
-// 	scanf( "%d", &val ) ;
-
 	if( ptr == NULL ){  //  || *top == -1
 		ptr = ( Stack* ) malloc( sizeof( Stack ) * 1 ) ;
 
@@ -141,8 +137,10 @@ node* search( node* root, int key ){
     
     return search( root-> right, key ) ;
 }
+
 node* min( node* root ){
     //  returns node with min element of tree
+    // printf( "in min" );
     if( root-> left == NULL )
         return root ;
     
@@ -154,51 +152,6 @@ node* max( node* root ){
         return root ;
     
     return max( root-> right ) ;
-}
-node* suc( node* root, int key, node* parent ){
-    if( root == NULL || root-> data == key ){
-        return root;
-    }
-    node* child = NULL, *ans = NULL;
-    
-    if( key < root-> data ){
-        child = root-> left;
-        ans = suc( root-> left, key, child ) ;
-    }
-    else{
-        child = root-> right;
-        ans = suc( root-> right, key, child ) ;
-    }
-    
-    if( ans != NULL ){
-        return ans;
-    }
-    // printf( "\nparent: %d", parent-> data );
-    // printf( "\nchild: %d", child-> data );
-    
-    if( child != parent-> right ){  //  child is left child of its parent
-        return parent;
-    }
-    
-    return NULL;
-}
-
-node* successor( node* root, node* arb ){   //  Any arbitary node in tree
-    //  returns next successor of arb node
-    //  Or returns just next bigger element than arb node's element
-    
-    if( arb-> right != NULL ){
-        return min( arb-> right ) ; //  If arb-> right != NULL then, successor is definitely min of right subtree
-    }
-    
-    //  now as arb-> right == NULL... Therefore no successor exist below this node
-    //  so we try to search above the tree
-    
-    
-    //  IF any node above arb node is not a right child of its Parent
-    //  then its parent is the next successor of arb node
-    node* parent = suc( root, arb-> data, root );
-    return parent ;
 }
 
 void insert( node** root, node* z ){
@@ -229,53 +182,110 @@ void insert( node** root, node* z ){
         parent-> right = z ;
     }
 }
-void replace( node** root, node* u, node* v ){
-    //  puts vth node at uth node's place
-    //  but doesn't take care of v's left and right
-    
-    if( u->p == NULL ){              //  u is the root node
-        *root = v ;
+
+node* suc( node* root, int key, node* succ ){
+    if( root == NULL )  return NULL;
+
+    if( root-> data > key ){    //  if true, this can be a possible successor
+        succ = root;
+        return suc( root-> left, key, succ ) ;
     }
-    else if( u == u-> p-> left ){    //  u is left child of it's parent
-        u-> p-> left = v ;
+    else if( root-> data < key ) {  //  if true, as data in node is already < key, so successor not possible
+        return suc( root-> right, key, succ ) ;
     }
-    else{                            //  u is right child of it's parent
-        u-> p-> right = v ;
+    else{
+        //  At this point, key == root-> data
+        return succ;
     }
-    if( v != NULL )
-        v-> p = u-> p ;              //  v's new parent is u's parent
+    // printf( "\nparent: %d", parent-> data );
+    // printf( "\nchild: %d", child-> data );
 }
-// void delete( node** root, node* z ){
-//     //  deletes zth node from tree
+
+node* successor( node* root, node* arb ){   //  Any arbitary node in tree
+    //  returns inorder successor of arb node
+    //  Or 
+    //  returns just next bigger element than arb node's element
     
-//     //  case 1: 
-//         //  if z.left == NULL , if the node to delete has no left child
-//     if( z-> left == NULL ){
-//         replace( root, z, z-> right ) ;
-//     }
-        
-//     //  case 2: 
-//         //  if z.right == NULL , if the node to delete has no right child
-//     else if( z-> right == NULL ){
-//         replace( root, z, z-> left ) ;
-//     }
+    printf( "\nIn Succ\n" );
+    if( arb-> right != NULL ){
+        return min( arb-> right ) ; //  If arb-> right != NULL then, successor is definitely min of right subtree
+    }
     
-//     //  case 3: 
-//         //  if the node to delete has both left & right child
-//     else{
-//         node* y = successor( z ) ;
-//         //  now as y is Successor/min so it's left will always be NULL
-        
-//         if( y != z-> right ){    //  If Successor != z.right
-//             replace( root, y, y-> right ) ;
-//             y-> right = z-> right ;
-//             y-> right-> p = y ;
-//         }
-//         replace( root, z, y ) ;
-//         y-> left = z-> left ;
-//         y-> left-> p = y ;
+    //  now as arb-> right == NULL... Therefore no successor exist below this node
+    //  As, left part(if exists) will always be < current val.
+    //  so we try to search above the tree
+    
+    return suc( root, arb-> data, NULL );
+}
+
+
+// void replace( node** root, node* u, node* v ){
+//     //  puts vth node at uth node's place
+//     //  but doesn't take care of v's left and right
+    
+//     if( u == *root ){              //  u is the root node
+//         *root = v ;
 //     }
+//     else if( u-> data < *root-> data ){    //  u is left child of it's parent
+//         u-> p-> left = v ;
+//     }
+//     else{                            //  u is right child of it's parent
+//         u-> p-> right = v ;
+//     }
+//     if( v != NULL )
+//         v-> p = u-> p ;              //  v's new parent is u's parent
 // }
+
+node* delete( node* root, int key ){
+    
+    //  case 1:
+    if( root == NULL )     return NULL;
+    
+    else if( root-> data < key ){ 
+        //  Assign right part of z to
+        root-> right = delete( root-> right, key ) ;
+        return root;
+    }
+        
+    else if( root-> data > key ){ 
+        //  Assign left part of z to
+        root-> left = delete( root-> left, key ) ;
+        return root;
+    }
+    
+    //  At this point root-> data == key:
+    node* tree = NULL;
+    //  case 1: 
+        //  if root.left == NULL , if the node to delete has no left child
+    if( root-> left == NULL ){
+        tree = root-> right;
+        free( root );
+        return tree;
+    }
+
+    //  case 2: 
+        //  if root.right == NULL , if the node to delete has no right child
+    else if( root-> right == NULL ){
+        tree = root-> left;
+        free( root );
+        return tree;
+    }
+
+    //  case 3: 
+        //  if the node to delete has both left & right child
+        //  then,
+        //  find inorder successor of root node, and replace it with root & delete successor
+    else{
+        //  As root node(which has to be deleted) has 2 children, so doesn't matter what val of root node is given
+        //  Successor will always exist in right part of root node, so no need to go above current node
+        node* s = successor( NULL, root ) ;
+
+        root-> data = s-> data;
+
+        root-> right = delete( root-> right, s-> data );
+    } 
+    return root;
+}
 
 
 
@@ -291,10 +301,6 @@ int dfs( node* root, int key, Stack* stack, int top ){
     
     if( dfs( root-> left, key, stack, top ) != 0 )
         return 1;
-        
-    stack = pop( stack, &top );
-    
-    stack = push( stack, &top, root-> data );
     
     if( dfs( root-> right, key, stack, top ) != 0 )
         return 1;
@@ -328,29 +334,29 @@ int main(){
     node* x = NULL ;
     
     //  DFS
-    Stack *stk = NULL;
-    if( dfs( root, 14, stk, -1 ) )
-        printf( "\nfound" );
-    else
-        printf( "\nNot Found" );
+    // Stack *stk = NULL;
+    // if( dfs( root, 14, stk, -1 ) )
+    //     printf( "\nfound" );
+    // else
+    //     printf( "\nNot Found" );
     
     
     
     
     
     //  Deletion
-        // delete( &root, search( root, 5 ) ) ;
-        // printf( "\nInOrder: " ) ;
-        // inOrder( root ) ;
-    
+        delete( root, 10 ) ;
+        printf( "\nInOrder: " ) ;
+        inOrder( root ) ;
+        printf( "\n\n" );
     //  Successor
-        // x = root-> left ;
-        // printf( "\n%d\n", x-> data );
+        // x = root-> right-> right ;
+        // printf( "\nx-> data: %d\n", x-> data );
         // x = successor( root, x ) ;
         // if( x != NULL )
-        //     printf( "\nSuccessor: %d", x-> data );
+        //     printf( "\nSuccessor: %d\n", x-> data );
         // else
-        //     printf( "\nNo Successor" ) ;
+        //     printf( "\nNo Successor\n" ) ;
         
     //  Maximum
         // x = max( root ) ;
